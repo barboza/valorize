75e9224796c42aa2f58e91ae3c6b40ee9fd33991
Rafael Barboza <barboza.cardoso@gmail.com>
2014-10-15 10:54:55 -0300

    Change the structure and texts of the home page

--- app/observers/mixpanel_observer.rb
+++ app/observers/mixpanel_observer.rb
@@ -3,16 +3,16 @@ class MixpanelObserver < ActiveRecord::Observer
 
   def from_waiting_confirmation_to_confirmed(contribution)
     user = contribution.user
-    tracker.track(contribution.user.id.to_s, "Contribution confirmed", {
-      user_id: user.id.to_s,
-      created: user.created_at,
-      last_login: user.last_sign_in_at,
-      contributions: user.total_contributed_projects,
-      has_contributions: (user.total_contributed_projects > 0),
-      project: contribution.project.name,
-      payment_method: contribution.payment_method,
-      payment_choice: contribution.payment_choice
-    })
+    #tracker.track(contribution.user.id.to_s, "Contribution confirmed", {
+      #user_id: user.id.to_s,
+      #created: user.created_at,
+      #last_login: user.last_sign_in_at,
+      #contributions: user.total_contributed_projects,
+      #has_contributions: (user.total_contributed_projects > 0),
+      #project: contribution.project.name,
+      #payment_method: contribution.payment_method,
+      #payment_choice: contribution.payment_choice
+    #})
   end
   alias :from_pending_to_confirmed :from_waiting_confirmation_to_confirmed
 
